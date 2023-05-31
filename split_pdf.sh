5cf6c45979395c0b4e4b3eac3ccf8033754d8c8f
ave6990 <ave6990@ya.ru>
2023-05-31 18:03:45 +0500

    split_pdf.sh v2023-05-31

---
+++ split_pdf.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# author: Aleksandr Ermolaev
+# version: 2023-05-31
+
+DEPARTMENT=9
+ENGINEER=61
+YEAR=2023
+
+source_file=$1
+pages_count=`identify -format "%n\n" $source_file | head -1`
+let "files_count=$pages_count / $2" 
+let "pages_in_out_files=$2"
+
+if [ $# -lt 3 ]
+then
+    number=1
+else
+    let "start_number=$3"
+fi
+
+if [ $# -lt 4 ]
+then
+    direction=1
+else
+    direction=-1
+fi
+
+echo "Страниц в исходном документе: $pages_count"
+echo "Страниц в протоколе: $pages_in_out_files"
+echo "Документов: $files_count"
+
+for ((a=pages_in_out_files-1; a<=pages_count; a=a+pages_in_out_files ))
+do
+    let "s=$a-$pages_in_out_files+1"
+    echo "Обработка страниц $s-$a..."
+    convert $source_file[$s-$a] $DEPARTMENT-$ENGINEER-$start_number-$YEAR.pdf
+    let "start_number=$start_number+$direction"
+done
+
+echo "Выполнено!"
