f4b039221522527a8e821ce4f8ff82f340db042f
ave6990 <ave6990@ya.ru>
2023-06-01 09:18:02 +0500

    split_pdf with pdftk

--- split_pdf.sh
+++ split_pdf.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
+# dependency: pdftk imagemagick
 # author: Aleksandr Ermolaev
-# version: 2023-05-31
+# e-mail: ave6990@ya.ru
+# version: 2023-06-01
 
 DEPARTMENT=9
 ENGINEER=61
 YEAR=2023
-DENSITY=250
-DEPTH=4
+#DENSITY=250
+#DEPTH=4
 
 source_file=$1
 pages_count=`identify -format "%n\n" $source_file | head -1`
@@ -31,11 +33,16 @@ echo "Страниц в исходном документе: $pages_count"
 echo "Страниц в протоколе: $pages_in_out_files"
 echo "Документов: $files_count"
 
-for ((a=pages_in_out_files-1; a<=pages_count; a=a+pages_in_out_files ))
+for ((a=pages_in_out_files; a<=pages_count; a=a+pages_in_out_files ))
 do
     let "s=$a-$pages_in_out_files+1"
     echo "Обработка страниц $s-$a..."
-    convert -density $DENSITY -depth $DEPTH $source_file[$s-$a] $DEPARTMENT-$ENGINEER-$start_number-$YEAR.pdf
+
+    # imagemagick version
+    #convert -density $DENSITY -depth $DEPTH $source_file[$s-$a] $DEPARTMENT-$ENGINEER-$start_number-$YEAR.pdf
+
+    pdftk $source_file cat $s-$a output $DEPARTMENT-$ENGINEER-$start_number-$YEAR.pdf
+
     let "start_number=$start_number+$direction"
 done
 
