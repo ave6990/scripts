543acd92b6a2f3ab9614a82ef3cd1310f7c65226
ave6990 <ave6990@ya.ru>
2023-06-01 10:16:49 +0500

    release split_pdf.sh

--- split_pdf.sh
+++ split_pdf.sh
@@ -7,8 +7,14 @@
 DEPARTMENT=9
 ENGINEER=61
 YEAR=2023
+SCAN_DIRECTORY=/media/sf_Y_DRIVE/СКАНЫ\ РЕЗЕРВНОЕ\ КОПИРОВАНИЕ/2023/Ермолаев
 #DENSITY=250
 #DEPTH=4
+
+if [ ! -d "./scans" ]
+then
+    mkdir scans
+fi
 
 source_file=$1
 pages_count=`identify -format "%n\n" $source_file | head -1`
@@ -45,5 +51,16 @@ do
 
     let "start_number=$start_number+$direction"
 done
+
+if [ -d "$SCAN_DIRECTORY" ]
+then
+    echo "Резервное копирование..."
+    cp $DEPARTMENT-$ENGINEER-*-$YEAR.pdf $SCAN_DIRECTORY/
+else
+    echo "ВНИМАНИЕ!!! Папка резервного копирования недоступна!"
+    echo "Резервное копирование НЕ выполнено!"
+fi
+
+mv $DEPARTMENT-$ENGINEER-*-$YEAR.pdf ./scans/
 
 echo "Выполнено!"
